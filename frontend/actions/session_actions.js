b718512f75d8a813449978ec65cafd2c797d4b06
Melanie Davila <davilam91@gmail.com>
2017-02-14 13:40:29 -0500

    Add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,46 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export function receiveCurrentUser(currentUser) {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+}
+
+export function receiveErrors(errors) {
+  return {
+    type: RECEIVE_ERRORS,
+    errors
+  };
+}
+
+export function login(user) {
+  return function (dispatch) {
+    return APIUtil.login(user).then(function (user) {
+      return dispatch(receiveCurrentUser(user));
+    }, function (errors) {
+      return dispatch(receiveErrors(errors.responseJSON));
+    });
+  };
+}
+
+export function logout() {
+  return function (dispatch) {
+    return APIUtil.logout().then(function () {
+      return dispatch(receiveCurrentUser(null));
+    });
+  };
+}
+
+export function signup(user) {
+  return function (dispatch) {
+    return APIUtil.signup(user).then(function (user) {
+      return dispatch(receiveCurrentUser(user));
+    }, function (error) {
+      return dispatch(receiveErrors(error.responseJSON));
+    });
+  };
+}
