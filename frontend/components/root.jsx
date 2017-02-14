99fa0035fd3387c7ffe5d95c73d3b3fc7c8a6fe1
Melanie Davila <davilam91@gmail.com>
2017-02-14 16:56:45 -0500

    Update root and entry file

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import SignUpForm from './signup/signup_form';
+import LogInForm from './login/login_form';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
     <Router history={ hashHistory }>
-      <Route path="/" component={ App } />
+      <Route path="/" component={ App }>
+      <Route
+        path='/signup'
+        component={ SignUpForm }/>
+      <Route
+        path='/login'
+        component={ LogInForm }/>
+    </Route>
     </Router>
   </Provider>
 );
--- frontend/midsoul.jsx
+++ frontend/midsoul.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+// import { signup, login, logout } from './util/session_api_util';
+import { signup, login, logout } from './actions/session_actions';
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to Midsoul</h1>, root);
+    const store = configureStore();
+    window.store = store;
+    ReactDOM.render(<Root store={store}/>, root);
 });
+
+window.signup = signup;
+window.login = login;
+window.logout = logout;
