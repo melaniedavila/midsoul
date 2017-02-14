f990e8a964dc9b2f34bcd2b0bfacbd7dc46ef1a4
Melanie Davila <davilam91@gmail.com>
2017-02-14 11:29:42 -0500

    Add API::SessionsController

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(user_params)
+    if @user
+      log_in!(@user)
+      render 'api/users/show'
+    else
+      render json: { base: ['Invalid username/password'] }, status: 422
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout!
+      render 'api/users/show'
+    else
+      render json: { base: ['Not logged in'] }, status: 404
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
