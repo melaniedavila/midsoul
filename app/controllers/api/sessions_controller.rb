b536ce7bdf76162e8da56f59bc79be1e94144c7d
Melanie Davila <davilam91@gmail.com>
2017-02-14 13:18:22 -0500

    Add validates_email_format_of gem

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -21,6 +21,6 @@ class Api::SessionsController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :f_name, :l_name, :password, :image_url)
+    params.require(:user).permit(:email, :password)
   end
 end
