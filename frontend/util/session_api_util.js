096e98c4d57643d786dd3fcfb5babfdbc3f4c94a
Melanie Davila <davilam91@gmail.com>
2017-02-14 13:40:03 -0500

    Add session API util functions

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,22 @@
+export function signup(user) {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/users',
+    data: { user }
+  });
+}
+
+export function login(user) {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/session',
+    data: { user }
+  });
+}
+
+export function logout() {
+  return $.ajax({
+    method: 'DELETE',
+    url: '/api/session'
+  });
+}
