ac0a9b77038f0ecb82c5f6459348a0ce45388814
Melanie Davila <davilam91@gmail.com>
2017-02-15 15:22:36 -0500

    Add default prof pic

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,7 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      default_profile_pic!(@user) unless @user.image_url
       log_in!(@user)
       render :show
     else
@@ -12,5 +13,9 @@ class Api::UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:email, :f_name, :l_name, :password, :image_url)
+  end
+
+  def default_profile_pic!(user)
+    user.image_url = '../../assets/images/profile_pic/default_prof_pic.png'
   end
 end
