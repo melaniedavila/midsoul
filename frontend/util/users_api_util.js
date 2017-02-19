02c3b8f1e80e4963e42be8144f956e39ee99ec26
Melanie Davila <davilam91@gmail.com>
2017-02-19 18:46:21 -0500

    Add user API util functions

---
+++ frontend/util/users_api_util.js
@@ -0,0 +1,43 @@
+import { hashHistory } from 'react-router';
+
+export const fetchAllUsers = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/users'
+  });
+};
+
+export const fetchSingleUser = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/users/${id}`
+  });
+};
+
+export const createUser = (user) => {
+  // TODO: uncomment below when comments impemented
+  // user.comments = Object.keys(user.comments).map(idx => user.comments[idx]);
+  return $.ajax({
+    method: 'POST',
+    url: 'api/users/',
+    data: { user },
+    success: function (res) {
+      hashHistory.push(`/users/${res.id}`);
+    }
+  });
+};
+
+export const updateUser = (user) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `api/users/${user.id}`,
+    data: { user }
+  });
+};
+
+export const deleteUser = (id) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `api/users/${id}`
+  });
+};
