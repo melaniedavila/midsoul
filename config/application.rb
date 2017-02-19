9b51b1eda2c8a894d1b36adaeeb2c30434c48639
Melanie Davila <davilam91@gmail.com>
2017-02-18 23:55:58 -0500

    Config paperclip

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,18 @@ module Midsoul
     Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
       config.assets.paths << path
     end
+
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_protocol: 'http',
+      url: 's3_domain_url',
+      path: 'images/:class/:id.:style.:extension',
+      s3_host_name: ENV.fetch('AWS_S3_HOSTNAME'),
+      s3_credentials: {
+        bucket: "midsoul-#{Rails.env}",
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+      }
+    }
   end
 end
