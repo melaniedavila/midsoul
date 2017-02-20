8656db3b538e2827f923f8005665a2e6a762ec94
Melanie Davila <davilam91@gmail.com>
2017-02-19 22:25:03 -0500

    Add update and show actions to users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,9 +10,36 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user
+      render :show
+    else
+      render json: ['User does not exist'], status: 404
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:email, :f_name, :l_name, :password, :image_url)
+    params.require(:user).
+      permit(
+        :email,
+        :f_name,
+        :l_name,
+        :password,
+        :image_url,
+        :profile_picture
+      )
   end
 
   # def default_profile_pic!(user)
