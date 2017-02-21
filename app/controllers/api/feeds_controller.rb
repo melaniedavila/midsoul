9ded6b9dbcc8fdefbe634a227b013e24acf26aaa
Melanie Davila <davilam91@gmail.com>
2017-02-20 22:18:58 -0500

    Fix syntax error

--- app/controllers/api/feeds_controller.rb
+++ app/controllers/api/feeds_controller.rb
@@ -10,8 +10,8 @@ class Api::FeedsController < ApplicationController
   def feed_for(opts)
     if opts[:user_id]
       runs = Run.find_by(runner_id: opts[:user_id])
-      routes = Route.find_by(:creator_id: opts[:user_id])
-      
+      routes = Route.find_by(creator_id: opts[:user_id])
+
 
     elsif opts[:friend_ids]
       # get many users' activities
