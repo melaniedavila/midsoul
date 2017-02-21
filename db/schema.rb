be7ad2b867dd7ad5d42a1b8580702387a31be367
Melanie Davila <davilam91@gmail.com>
2017-02-20 22:10:30 -0500

    Update todos

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20170220220949) do
   add_index "friend_requests", ["requestor_id", "requestee_id"], name: "index_friend_requests_on_requestor_id_and_requestee_id", unique: true, using: :btree
 
   create_table "friendships", force: :cascade do |t|
-    t.integer "user_id",   null: false
-    t.integer "friend_id", null: false
+    t.integer  "user_id",    null: false
+    t.integer  "friend_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "friendships", ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true, using: :btree
@@ -74,4 +76,5 @@ ActiveRecord::Schema.define(version: 20170220220949) do
   add_index "users", ["f_name"], name: "index_users_on_f_name", using: :btree
   add_index "users", ["l_name"], name: "index_users_on_l_name", using: :btree
   add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
+
 end
