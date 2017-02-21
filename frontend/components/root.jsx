04780cab43db283f60b1240e8c8913eb701cee4d
Melanie Davila <davilam91@gmail.com>
2017-02-21 18:32:41 -0500

    Update routes to include FriendSearchContainer

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -17,9 +17,8 @@ import UserEditContainer from './users/form/user_edit_container';
 import UserShowContainer from './users/show/user_show_container';
 import FriendRequestIndexContainer from './friends/requests/friend_request_index_container';
 import FriendIndexContainer from './friends/index/friend_index_container';
+import FriendSearchContainer from './friends/search/friend_search_container';
 import CommunityContainer from './friends/community/community_container';
-
-
 
 export default function Root() {
   const preloadedState = window.currentUser ?
@@ -115,7 +114,7 @@ export default function Root() {
              component={ FriendIndexContainer }
              onEnter={ requireLogIn }/>
            { /* <Route path="find-friends"
-             component={ FindFriendsContainer }
+             component={ FriendSearchContainer }
              onEnter={ _redirectIfLoggedOut }/>*/}
            <Route path="friend-requests"
              component={ FriendRequestIndexContainer }
@@ -126,29 +125,3 @@ export default function Root() {
     </Provider>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <Route
-//   path="/community/"
-//   component={ CommunityTabsContainer }
-//   onEnter={ requireLogIn }/>
-// <Route
-//   path="/community/friends"
-//   component={ FriendIndexContainer }
-//   onEnter={ requireLogIn }/>
-// <Route
-//   path="/community/friend-requests"
-//   component={ FriendRequestIndexContainer }
-//   onEnter={ requireLogIn }/>
