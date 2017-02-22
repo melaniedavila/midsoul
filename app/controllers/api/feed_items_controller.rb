0f63a5d76ba8930e77c6f64be6a99bba351cc1c7
Melanie Davila <davilam91@gmail.com>
2017-02-21 23:30:11 -0500

    Update feeditems controller index action

--- app/controllers/api/feed_items_controller.rb
+++ app/controllers/api/feed_items_controller.rb
@@ -1,8 +1,20 @@
 class Api::FeedItemsController < ApplicationController
   def index
+    user_ids = single_user_id || many_user_ids
+
     @feed_items = FeedItem.
       order(created_at: 'DESC').
-      where(user_id: params[:user_ids]).
+      where(user_id: user_ids).
       includes(:feedable)
   end
+
+  private
+
+    def single_user_id
+      params[:user_id]
+    end
+
+    def many_user_ids
+      current_user.friends.pluck(:id)
+    end
 end
