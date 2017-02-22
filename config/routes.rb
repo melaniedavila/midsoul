405011bbab77049dad3e45a3f198bf381c82d758
Melanie Davila <davilam91@gmail.com>
2017-02-21 23:30:25 -0500

    Nest feed items route under user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
-    resources :users, only: [:create, :update, :show, :index]
+    resources :users, only: [:create, :update, :show, :index] do
+      resources :feed_items, only: [:index]
+    end # /users/:id/feed_items
     resources :routes, only: [:create, :show, :destroy, :update, :index]
     resources :runs, only: [:create, :show, :destroy, :update, :index]
     resources :feed_items, only: [:index]
