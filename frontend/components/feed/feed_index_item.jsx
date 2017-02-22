a4ccf683e60b2afea34ede2b235b39f60bf14d55
Melanie Davila <davilam91@gmail.com>
2017-02-22 16:09:20 -0500

    Add links to user, route, and run show pages

--- frontend/components/feed/feed_index_item.jsx
+++ frontend/components/feed/feed_index_item.jsx
@@ -3,18 +3,24 @@ import { Link } from 'react-router';
 
 const FeedIndexItem = ({ feedItem }) => {
   let feedIndexItemDetails;
+  debugger
+
   if (feedItem.feedable_type === 'Route') {
 
     feedIndexItemDetails = (
       <div className='feed-index-item-container'>
         <aside className='feed-index-item-map'>
-          <img src={`https://maps.googleapis.com/maps/api/staticmap?size=400x400&path=color:0x0c5d94%7Cenc:${feedItem.feedable.polyline}&key=${window.googleMapsApiKey}`}
-                alt='Map'></img>
+          <Link to={`/routes/${feedItem.feedable.id}`}>
+            <img src={`https://maps.googleapis.com/maps/api/staticmap?size=400x400&path=color:0x0c5d94%7Cenc:${feedItem.feedable.polyline}&key=${window.googleMapsApiKey}`}
+                  alt='Map'></img>
+          </Link>
         </aside>
         <main className='activity-details-flex-container'>
           <div className='mini-activity-description-flex-container'>
             <div className='user-thumb-container'>
-              <img src={feedItem.feedable.creator.profile_picture} alt='Profile picture'></img>
+              <Link to={`/users/${feedItem.feedable.creator.id}`}>
+                <img src={feedItem.feedable.creator.profile_picture} alt='Profile picture'></img>
+              </Link>
             </div>
             <h3>{feedItem.feedable.creator.f_name} created the route {feedItem.feedable.title}</h3>
           </div>
@@ -25,16 +31,21 @@ const FeedIndexItem = ({ feedItem }) => {
       </div>
     );
   } else {
+    debugger
     feedIndexItemDetails = (
       <div className='feed-index-item-container'>
         <aside className='feed-index-item-map'>
-          <img src={`https://maps.googleapis.com/maps/api/staticmap?size=400x400&path=color:0x0c5d94%7Cenc:${feedItem.feedable.route.polyline}&key=${window.googleMapsApiKey}`}
-                alt='Map'></img>
+            <Link to={`/runs/${feedItem.feedable.id}`}>
+              <img src={`https://maps.googleapis.com/maps/api/staticmap?size=400x400&path=color:0x0c5d94%7Cenc:${feedItem.feedable.route.polyline}&key=${window.googleMapsApiKey}`}
+                    alt='Map'></img>
+            </Link>
         </aside>
         <main className='activity-details-flex-container'>
           <div className='mini-activity-description-flex-container'>
             <div className='user-thumb-container'>
-              <img src={feedItem.feedable.runner.profile_picture} alt='Profile picture'></img>
+              <Link to={`/users/${feedItem.feedable.runner.id}`}>
+                <img src={feedItem.feedable.runner.profile_picture} alt='Profile picture'></img>
+              </Link>
             </div>
             <h3>{feedItem.feedable.runner.f_name} ran {feedItem.feedable.route.distance} miles at a pace of {((feedItem.feedable.duration / 60) / feedItem.feedable.route.distance).toFixed(2)} mins/mi</h3>
           </div>
