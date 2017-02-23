e777cf7f48c5bdbb048d73e7f41e46cf648978ba
Melanie Davila <davilam91@gmail.com>
2017-02-23 12:04:41 -0500

    Add comments index to feed

--- frontend/components/feed/feed_index_item.jsx
+++ frontend/components/feed/feed_index_item.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import CommentIndex from '../comments/index/comment_index';
 
 const FeedIndexItem = ({ feedItem }) => {
   let feedIndexItemDetails;
@@ -25,8 +26,7 @@ const FeedIndexItem = ({ feedItem }) => {
             <h3>{feedItem.feedable.creator.f_name} created the route {feedItem.feedable.title}</h3>
           </div>
           <div className='activity-comments'>
-            Enter Comments Here
-            {/*}<p>{feedItem.feedable.comments}</p>*/}
+            <CommentIndex comments={feedItem.feedable.comments}/>
           </div>
         </main>
       </div>
@@ -50,8 +50,7 @@ const FeedIndexItem = ({ feedItem }) => {
             <h3>{feedItem.feedable.runner.f_name} ran {feedItem.feedable.route.distance} miles at a pace of {((feedItem.feedable.duration / 60) / feedItem.feedable.route.distance).toFixed(2)} mins/mi</h3>
           </div>
           <div className='activity-comments'>
-            Enter Comments Here
-            {/*}<p>{feedItem.feedable.comments}</p>*/}
+            <CommentIndex comments={feedItem.feedable.comments}/>
           </div>
         </main>
       </div>
