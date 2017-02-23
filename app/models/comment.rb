cfdb2a542702492fce4354b1dbd76b983eee64f6
Melanie Davila <davilam91@gmail.com>
2017-02-22 21:02:05 -0500

    Create comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  author_id        :integer          not null
+#  commentable_id   :integer          not null
+#  commentable_type :string           not null
+#  body             :text             not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+class Comment < ActiveRecord::Base
+  validates :author_id, :commentable_id, :commentable_type,
+            :body, presence: true
+
+  belongs_to(
+    :author,
+    primary_key: :id,
+    foreign_key: :author_id,
+    class_name: 'User')
+
+  belongs_to :commentable, polymorphic: true
+end
