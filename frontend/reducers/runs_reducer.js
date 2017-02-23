82b57627d8dea392016bd84d5fcce5fbdcd94afb
Melanie Davila <davilam91@gmail.com>
2017-02-23 10:41:37 -0500

    Update runs reducer

--- frontend/reducers/runs_reducer.js
+++ frontend/reducers/runs_reducer.js
@@ -1,6 +1,7 @@
 import { merge, assign } from 'lodash';
 import {  RECEIVE_ALL_RUNS, RECEIVE_SINGLE_RUN, REMOVE_RUN,
           RECEIVE_NEW_RUN, RECEIVE_RUN_ERRORS } from '../actions/runs_actions';
+import { RECEIVE_NEW_RUN_COMMENT, REMOVE_RUN_COMMENT } from '../actions/comments_actions';
 
 export default function runsReducer(state = {}, action) {
   Object.freeze(state);
@@ -14,8 +15,21 @@ export default function runsReducer(state = {}, action) {
       let newState = merge({}, state);
       delete newState[action.run.id];
       return newState;
-    // case RECEIVE_NEW_RUN_COMMENT:
-    // case REMOVE_RUN_COMMENT:
+    case RECEIVE_NEW_RUN_COMMENT:
+    // goal: look at commentable_id to find run from the state. concat new comment
+    // into comments arr
+      debugger
+      const comment = action.comment;
+      const commentable_id = action.comment.commentable_id;
+      const run = state.runs.commentable_id;
+      const comments = Object.assign({}, run.comments.concat(comments));
+      return Object.assign({}, state, { comments: comments} );
+    case REMOVE_RUN_COMMENT:
+      let runComments = merge({}, state.run.comments);
+      const commentToRemove = action.comment;
+      const runCommentableId = action.comment.commentable_id;
+      const newComments = comments.filter((comment) => comment.id !== commentToRemove.id);
+      return Object.assign({}, state, {comments: newComment });
     default:
       return state;
   }
