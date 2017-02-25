76244c3edb90b04e0902d6b84a530f595baef1b6
Melanie Davila <davilam91@gmail.com>
2017-02-25 14:00:10 -0500

    Add footer component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import NavigationContainer from './navigation/navigation_container';
+import Footer from './footer/footer';
 
 const App = function (props) {
   return(
-    <div>
-      <NavigationContainer />
-      { props.children }
+    <div className='wrap'>
+      <div className='app-nav-and-content-container'>
+          <NavigationContainer />
+          { props.children }
+      </div>
+      <div className='app-footer'>
+        <Footer />
+      </div>
     </div>
   );
 };
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <footer className='footer-container'>
+          <h3>Melanie Davila</h3>
+          <ul>
+            <li><i className="fa fa-envelope fa-lg" aria-hidden="true"></i><a href='mailto:davilam91@gmail.com' className='footer-link'>davilam91@gmail.com</a></li>
+            <li><i className="fa fa-github fa-lg" aria-hidden="true"></i><a target="_blank" href="https://github.com/melaniedavila" className='footer-link'>Github</a></li>
+            <li><i className="fa fa-linkedin-square fa-lg" aria-hidden="true"></i><a target="_blank" href="https://linkedin.com/in/melaniedavila" className='footer-link'>LinkedIn</a></li>
+          </ul>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
