cd482bb2668676b85657ee0673e00a09e61ead00
Melanie Davila <davilam91@gmail.com>
2017-03-06 18:23:17 -0500

    Orient contact info footer horizontally

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,8 +5,8 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className='footer-container'>
-          <h3>Melanie Davila</h3>
           <ul>
+            <li>Melanie Davila</li>
             <li><i className="fa fa-envelope fa-lg" aria-hidden="true"></i><a href='mailto:davilam91@gmail.com' className='footer-link'>davilam91@gmail.com</a></li>
             <li><i className="fa fa-github fa-lg" aria-hidden="true"></i><a target="_blank" href="https://github.com/melaniedavila" className='footer-link'>Github</a></li>
             <li><i className="fa fa-linkedin-square fa-lg" aria-hidden="true"></i><a target="_blank" href="https://linkedin.com/in/melaniedavila" className='footer-link'>LinkedIn</a></li>
