be8b73b10e84c71b0f1eaaec040f88c7900fb234
Melanie Davila <davilam91@gmail.com>
2017-03-13 11:33:19 -0400

    Add portolio site link to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -6,10 +6,39 @@ class Footer extends React.Component {
     return (
       <footer className='footer-container'>
           <ul>
-            <li>Melanie Davila</li>
-            <li><i className="fa fa-envelope fa-lg" aria-hidden="true"></i><a href='mailto:davilam91@gmail.com' className='footer-link'>davilam91@gmail.com</a></li>
-            <li><i className="fa fa-github fa-lg" aria-hidden="true"></i><a target="_blank" href="https://github.com/melaniedavila" className='footer-link'>Github</a></li>
-            <li><i className="fa fa-linkedin-square fa-lg" aria-hidden="true"></i><a target="_blank" href="https://linkedin.com/in/melaniedavila" className='footer-link'>LinkedIn</a></li>
+            <li>
+              <a  target="_blank"
+                  href="http://www.melaniedavila.com"
+                  className='footer-link'>
+                  Melanie Davila</a>
+            </li>
+            <li>
+              <i  className="fa fa-envelope fa-lg"
+                  aria-hidden="true">
+              </i>
+              <a  href='mailto:davilam91@gmail.com'
+                  className='footer-link'>
+                  davilam91@gmail.com
+              </a>
+            </li>
+            <li>
+              <i  className="fa fa-github fa-lg"
+                  aria-hidden="true">
+              </i>
+              <a  target="_blank"
+                  href="https://github.com/melaniedavila"
+                  className='footer-link'>Github
+              </a>
+            </li>
+            <li>
+              <i  className="fa fa-linkedin-square fa-lg"
+                  aria-hidden="true">
+              </i>
+              <a  target="_blank"
+                  href="https://linkedin.com/in/melaniedavila"
+                  className='footer-link'>LinkedIn
+              </a>
+            </li>
           </ul>
       </footer>
     );
