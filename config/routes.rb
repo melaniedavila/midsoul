c86a61f6c8f0d68d71cbac750473ca6b6242ee54
Melanie Davila <davilam91@gmail.com>
2017-03-16 14:35:34 -0400

    Reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,23 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
+    resources :comments, only: [:destroy]
+    resources :feed_items, only: [:index]
+    resources :friend_requests, only: [:index, :create, :update]
+    resources :friendships, only: [:create, :destroy]
+    resources :friendships, only: [:index], path: 'friends'
     resource :session, only: [:create, :destroy]
-    resources :users, only: [:create, :update, :show, :index] do
-      resources :feed_items, only: [:index]
-    end
+
     resources :routes, only: [:create, :show, :destroy, :update, :index] do
       resources :comments, only: [:index, :create]
     end
+
     resources :runs, only: [:create, :show, :destroy, :update, :index] do
       resources :comments, only: [:index, :create]
     end
-    resources :feed_items, only: [:index]
-    resources :friendships, only: [:index], path: 'friends'
-    resources :friendships, only: [:create, :destroy]
-    resources :friend_requests, only: [:index, :create, :update]
-    resources :comments, only: [:destroy]
+
+    resources :users, only: [:create, :update, :show, :index] do
+      resources :feed_items, only: [:index]
+    end
   end
 end
