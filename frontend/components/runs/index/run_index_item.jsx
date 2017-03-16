79b1add56ce1ebf550b8afce209139b76b00fd26
Melanie Davila <davilam91@gmail.com>
2017-03-16 14:06:31 -0400

    Modify run date format on run index page

--- frontend/components/runs/index/run_index_item.jsx
+++ frontend/components/runs/index/run_index_item.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const RunIndexItem = ({ run }) => {
+  const runMonth = run.date.slice(5, 7);
+  const runDay = run.date.slice(8);
+  const runYear = run.date.slice(2, 4);
+
   return (
   <li className="run-index-item">
-    <span className='run-date'>{run.date}</span>
+    <span className='run-date'>{runMonth}-{runDay}-{runYear}</span>
     <Link to={`/runs/${run.id}`}>
       <img src={`https://maps.googleapis.com/maps/api/staticmap?size=300x150&path=color:0x0c5d94%7Cenc:${run.route.polyline}&key=${window.googleMapsApiKey}`}
             alt={run.route.title}></img>
