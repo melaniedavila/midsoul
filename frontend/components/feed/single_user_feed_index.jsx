e65475c417ee444ede65682b56b4785150afdb67
Melanie Davila <davilam91@gmail.com>
2017-03-19 15:33:50 -0400

    Add lifecycle hook to single user feed index to fix bug

--- frontend/components/feed/single_user_feed_index.jsx
+++ frontend/components/feed/single_user_feed_index.jsx
@@ -5,7 +5,16 @@ import FeedIndexItem from './feed_index_item';
 export default class SingleUserFeedIndex extends React.Component {
   componentDidMount() {
     const userId = this.props.userId;
+    console.log('userId', this.props.userId);
     this.props.requestSingleUserFeedItems(userId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('nextProps', nextProps);
+    if (this.props.userId !== nextProps.userId) {
+      nextProps.requestSingleUserFeedItems(nextProps.userId)
+    }
+    // this.feedItem = nextProps.feedItem;
   }
 
   render() {
