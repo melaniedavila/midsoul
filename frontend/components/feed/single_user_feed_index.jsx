3273c85d48706bad1aefd6b6e1a065a443317887
Melanie Davila <davilam91@gmail.com>
2017-03-19 20:40:40 -0400

    Sort single user feed index in reverse chronological order

--- frontend/components/feed/single_user_feed_index.jsx
+++ frontend/components/feed/single_user_feed_index.jsx
@@ -5,12 +5,10 @@ import FeedIndexItem from './feed_index_item';
 export default class SingleUserFeedIndex extends React.Component {
   componentDidMount() {
     const userId = this.props.userId;
-    console.log('userId', this.props.userId);
     this.props.requestSingleUserFeedItems(userId);
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log('nextProps', nextProps);
     if (this.props.userId !== nextProps.userId) {
       nextProps.requestSingleUserFeedItems(nextProps.userId)
     }
@@ -23,9 +21,8 @@ export default class SingleUserFeedIndex extends React.Component {
     } else {
       return (
       <section className="feed-index">
-        <h2>Activity Feed</h2>
         <ul className='feed-items-list'>
-          {feedItems.map((feedItem, idx) => <FeedIndexItem key={idx} feedItem={feedItem} />)}
+          {feedItems.reverse().map((feedItem, idx) => <FeedIndexItem key={idx} feedItem={feedItem} />)}
         </ul>
       </section> );
     }
