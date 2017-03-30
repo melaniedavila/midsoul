ed75f3648efff2101e744ca92b4d2e323a3a306a
Melanie Davila <davilam91@gmail.com>
2017-03-30 14:26:19 -0400

    Replace line breaks with scss in sign up form component

--- frontend/components/signup/signup_form.jsx
+++ frontend/components/signup/signup_form.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import { Link } from 'react-router';
 import ErrorsList from '../errors/errors_list';
 import FeatureFooter from '../footer/feature_footer';
-import React from 'react';
 import SplashVideo from '../splash/splashVideo';
 
 export default class SignUpForm extends React.Component {
   constructor(props){
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = { email: '', f_name:'', l_name:'', password: '' };
   }
 
@@ -20,54 +21,47 @@ export default class SignUpForm extends React.Component {
 
   handleSubmit(e){
     e.preventDefault();
-    this.props.signUp(this.state).then(() => this.props.router.push('/'));
+    const { router, signUp } = this.props;
+    signUp(this.state).then(() => router.push('/'));
   }
 
   render(){
-    const errors = this.props.errors;
+    const { errors } = this.props;
     let errorsList;
     if (errors) {
       errorsList = <div className='errors-list'>
-                    <ErrorsList errors={ errors.base } />
-                  </div>
+                    <ErrorsList errors={errors.base} />
+                  </div>;
     }
 
     return(
       <section>
         <div className='signup-form-container'>
-          <form onSubmit={this.handleSubmit.bind(this)}>
-            <br />
+          <form onSubmit={this.handleSubmit}>
             <h4>Create a midSoul account</h4>
-            <br />
-            {errorsList}
-            <br />
+            { errorsList }
             <input type='text'
               value={this.state.f_name}
               placeholder={'First Name'}
               onChange={this.update('f_name')}/>
-            <br />
 
             <input type='text'
               value={this.state.l_name}
               placeholder={'Last Name'}
               onChange={this.update('l_name')}/>
-            <br />
 
             <input type='text'
               value={this.state.email}
               placeholder={'Email'}
               onChange={this.update('email')}/>
-            <br />
 
             <input type='password'
               placeholder={'Password'}
               onChange={this.update('password')}/>
-            <br />
 
-            <br />
-
-            <input type='submit' value='JOIN NOW'/>
-            <h6>Already have an account?  <Link to='/login'>Log In</Link></h6>
+            <input type='submit' value='Join Now'/>
+            <h6>Already have an account?  <Link to='/login'>Log In
+                                          </Link></h6>
           </form>
         </div>
         <div className='splash-video'>
